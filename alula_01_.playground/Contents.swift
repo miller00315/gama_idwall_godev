472f5500c08f197e51d7eefaec5e535f32700019
Miller César de Oliveira <miller00315@gmail.com>
2022-03-07 19:47:01 -0300

    Äjustar git hub

--- alula_01_.playground/Contents.swift
+++ alula_01_.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// var = mutable
+var name = "Miller"
+
+var lastName = "Oliveira"
+
+// let = not mutable
+let age = 31
+
+let res = "Meu nome é: \(name) \(lastName), minha idade é: \(age)"
+
+print(res)
+
+/*
+ Tipos de variaveis
+ 
+ Int = inteiro,
+ Int ou Int32
+ Int ou Int64
+ Double =
+ Float = 
+ */
+
