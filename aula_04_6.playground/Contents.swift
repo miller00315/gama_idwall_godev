c157d5d4cea18f19b7b243ecebb711bf9195659c
Miller César de Oliveira <miller00315@gmail.com>
2022-03-10 21:47:06 -0300

    Äjustando problemas

---
+++ aula_04_6.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+let color = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1.0)
+
+let lightBlue: UIColor = #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1) // Literal #colorLiteral
+
+print(lightBlue)
+
+
---
+++ aula_04_7.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+/*
+ Programacao orientada a protocolos
+ */
+
+protocol ReformarCampo {
+    func plantarNovaGrama()
+    
+    func pintarNovasLinhas()
+}
+
+class EmpresaA: ReformarCampo {
+    func plantarNovaGrama() {
+        
+    }
+    
+    func pintarNovasLinhas() {
+        
+    }
+}
+
+
+class EmpresaB: ReformarCampo {
+    func plantarNovaGrama() {
+        
+    }
+    
+    func pintarNovasLinhas() {
+        
+    }
+}
